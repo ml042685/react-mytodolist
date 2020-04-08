10058e8ec58d1011531212d5b84c9e178c9decd3
Marcos Luna <ml042685@yahoo.com>
2020-04-07 23:44:00 -0500

    production

--- server.js
+++ server.js
@@ -32,9 +32,14 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.get('*', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function(req, res) {
   res.sendFile(path.join(_dirname, '/client/build', 'index.html'));
 });
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
